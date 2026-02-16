189b1774c748ffe63ab3661076a466af3bf2f670
NguyenMinh1301 <nguyenminh1301.dev@gmail.com>
2026-02-17 02:06:24 +0700

    refactor(app): set default timezone to Asia/Ho_Chi_Minh

--- src/main/java/com/vietrecruit/Application.java
+++ src/main/java/com/vietrecruit/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+
+		System.setProperty("user.timezone", "Asia/Ho_Chi_Minh");
+
 		SpringApplication.run(Application.class, args);
 	}
 
