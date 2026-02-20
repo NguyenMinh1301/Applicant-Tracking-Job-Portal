1877f2ab91eee8dadaa08e6698d5aa16fa5296b9
NguyenMinh1301 <nguyenminh1301.dev@gmail.com>
2026-02-21 02:35:13 +0700

    chore(test): configure testcontainers and application-test.yml for integration tests

--- src/test/java/com/vietrecruit/ApplicationTests.java
+++ src/test/java/com/vietrecruit/ApplicationTests.java
@@ -2,9 +2,19 @@ package com.vietrecruit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class ApplicationTests {
+
+    @Container @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("pgvector/pgvector:pg15");
 
     @Test
     void contextLoads() {}
